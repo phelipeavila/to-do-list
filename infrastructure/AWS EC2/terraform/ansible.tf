2a86f3ed9f2734c8aa05ad2a818c9f636c9e4fe8
phelipe <phelipe.avila@gmail.com>
2023-08-16 15:01:20 -0300

    terraform workflow

--- infrastructure/AWS EC2/terraform/ansible.tf
+++ infrastructure/AWS EC2/terraform/ansible.tf
@@ -127,7 +127,8 @@ module "sg_managed_nodes" {
 
 data "aws_ami" "private_ec2" {
   most_recent = true
-  name_regex = "^al2023-ami-2023*"
+  # name_regex = "^al2023-ami-2023*"
+  name_regex = "^ubuntu/images/hvm-ssd/ubuntu-jammy-22.04*"
   owners = ["amazon"]
   
   filter {
