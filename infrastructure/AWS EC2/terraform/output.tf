10bc979e88c998d0aea13d5a4cba1a7bb4eef055
phelipe <phelipe.avila@gmail.com>
2023-08-24 16:11:48 -0300

    infra: update output info

--- infrastructure/AWS EC2/terraform/output.tf
+++ infrastructure/AWS EC2/terraform/output.tf
@@ -2,14 +2,30 @@ output "database_ext_ip" {
   value = module.ec2_database.public_ip
 }
 
+output "database_priv_ip" {
+  value = module.ec2_database.private_ip
+}
+
 output "backend_ext_ip" {
   value = module.ec2_backend.public_ip
+}
+
+output "backend_priv_ip" {
+  value = module.ec2_backend.private_ip
 }
 
 output "frontend_ext_ip" {
   value = module.ec2_frontend.public_ip
 }
 
+output "frontend_priv_ip" {
+  value = module.ec2_frontend.private_ip
+}
+
 output "control_node_ext_ip" {
   value = module.ec2_control_node.public_ip
+}
+
+output "control_node_priv_ip" {
+  value = module.ec2_control_node.private_ip
 }
